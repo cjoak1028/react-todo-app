4e8396b7f46ec7c9eda954b45a2dd8475be2d47b
David Kim <cjoak1028@gmail.com>
2021-09-02 14:40:26 -0400

    added edit feature

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,60 @@
-import React from 'react'
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText, Modal, Button } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import './Todo.css'
 import db from './firebase';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, setDoc } from "firebase/firestore";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 function Todo(props) {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [input, setInput] = useState('');
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const updateTodo = () => {
+        // update the todo with the new input text
+        const todoRef = doc(db, "todos", props.todo.id);
+        setDoc(todoRef, { 
+            todo: input
+        }, { merge: true })
+        setOpen(false);
+    }
+
     return (
+        <>
+        <Modal
+            open={open}
+            onClose={e => setOpen(false)}
+        >
+            <div className={classes.paper}>
+                <h1>I am a modal</h1>
+                <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)}/>
+                <Button onClick={e => updateTodo()}>Update Todo</Button>
+            </div>
+        </Modal>
         <List className="todo__list">
             <ListItem>
                 <ListItemText primary={props.todo.todo} secondary="Dummy Deadline ⏰ " />
             </ListItem>
+            <button onClick={e => setOpen(true)}>Edit</button>
             <DeleteForeverIcon onClick={event => deleteDoc(doc(db, "todos", props.todo.id))}/>
         </List>
+        </>
     )
 }
 
